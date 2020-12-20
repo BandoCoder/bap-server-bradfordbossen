39065a08f95b3eecafe93a03c2f99d98ef979355
kittybagpipes <sparticus978@gmail.com>
2020-12-19 23:27:41 -0800

    setup cors

--- src/config.js
+++ src/config.js
@@ -3,4 +3,5 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || "development",
   DATABASE_URL: process.env.DATABASE_URL || "postgresql://bfart@localhost/bap",
   JWT_SECRET: process.env.JWT_SECRET || "change-this.secret",
+  CLIENT_ORIGIN: "bap-capstone1-bradfordbosen.vercel.app",
 };
