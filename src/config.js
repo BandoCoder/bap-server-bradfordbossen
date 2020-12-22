82981dd1ee4ec6aa83a28e865a4879acaccf1100
kittybagpipes <sparticus978@gmail.com>
2020-12-21 19:03:31 -0800

    latest release url

--- src/config.js
+++ src/config.js
@@ -3,5 +3,5 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || "development",
   DATABASE_URL: process.env.DATABASE_URL || "postgresql://bfart@localhost/bap",
   JWT_SECRET: process.env.JWT_SECRET || "change-this.secret",
-  CLIENT_ORIGIN: "https://bap-capstone1-bradfordbosen.bandocoder.vercel.app",
+  CLIENT_ORIGIN: "https://bap-capstone1-bradfordbosen-3ynju1gov.vercel.app",
 };
