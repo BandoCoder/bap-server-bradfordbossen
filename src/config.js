54de411426019340b2109c712ee6cde42b796163
kittybagpipes <sparticus978@gmail.com>
2020-12-28 09:53:44 -0800

    ready deploy

--- src/config.js
+++ src/config.js
@@ -4,5 +4,4 @@ module.exports = {
   DATABASE_URL: process.env.DATABASE_URL || "postgresql://bfart@localhost/bap",
   JWT_SECRET: process.env.JWT_SECRET || "change-this.secret",
   JWT_EXPIRY: process.env.JWT_EXPIRY || "20s",
-  CLIENT_ORIGIN: "http://localhost:3000",
 };
